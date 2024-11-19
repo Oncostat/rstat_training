8d320cfc29cc0b5e111db7e33ef17704f94af378
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2024-11-19 15:49:34 +0100

    tous les codes et callout en fragments

--- modules/4_analyse/assets/custom.js
+++ modules/4_analyse/assets/custom.js
@@ -1,8 +1,38 @@
+
 console.log("custom.js")
 
 const $ = (selector) => document.querySelectorAll(selector);
 
+const selectNonNested = function(className) {
+    const cleanClassName = className.startsWith('.') ? className : `.${className}`;
+    return document.querySelectorAll(`${cleanClassName}:not(${cleanClassName} ${cleanClassName})`);
+}
+
+
+const init = function(event) {
+
+  console.log("custom.js -> init")
+
+  // tous les chunks et leur output deviennent des fragments
+  code_chunks = selectNonNested(".sourceCode")
+  code_chunks.forEach(element => {
+      element.classList.add('fragment');
+  });
+
+  code_output = selectNonNested(".cell-output-display")
+  code_output.forEach(element => {
+      element.classList.add('fragment');
+  });
+
+  // tous les callouts deviennent des fragments non incrémentaux
+  callouts = $(".callout")
+  callouts.forEach(element => {
+      element.classList.add('fragment', 'nonincremental');
+  });
+
+}
 window.addEventListener("ready", (event) => {
   console.log("EVENT: window ready")
+  init()
 });
 
