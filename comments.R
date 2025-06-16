6c7c6d43d9bb73e8e27857852723d2bf89a3dbad
Nusaibah Ibrahimi <nusaibah.ibr@gmail.com>
2025-06-16 10:34:48 +0200

    Revert "update module3 and annexe files"
    This reverts commit e0775453784941f61ecb5d4791573ef640086b8f.

--- comments.R
+++
@@ -1,8 +0,0 @@
-# Title1 ----  
-# a title of level 1
-
-## Title2 ----  
-# a title of level 2
-
-### Title3 ----  
-# a title of level 3
--- plot.R
+++
@@ -1,4 +0,0 @@
-library(ggplot2)
-ggplot(data = mtcars,
-       aes(x = disp, y = hp))+
-  geom_point()
